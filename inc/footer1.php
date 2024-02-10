0175821a6a8e0a69f7ad5edc538fef1f76af9389
PRAVEENA L <122954486+Pravzzz-7@users.noreply.github.com>
2024-02-10 08:52:55 +0530

    Update footer1.php

--- inc/footer1.php
+++ inc/footer1.php
@@ -1,3 +1,3 @@
 <footer class="footer footer-alt">
-    2020 - <?php echo date('Y'); ?> &copy; Hospital Management System. Developed By G Ganesh S P </a>
+    2020 - <?php echo date('Y'); ?> &copy; Hospital Management System. Developed By L PRAVEENA (1VE20CS074)  </a>
 </footer>
