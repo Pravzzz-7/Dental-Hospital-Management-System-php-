00e71810b85483ae6a3832d8ba983141d53dd89c
PRAVEENA L <122954486+Pravzzz-7@users.noreply.github.com>
2024-02-10 08:37:26 +0530

    Add files via upload

--- his_admin_register.php
+++ his_admin_register.php
@@ -1,40 +1,45 @@
+<!--Server side code to handle  sign up-->
 <?php
-    session_start();
-    include('assets/inc/config.php');//get configuration file
-    if(isset($_POST['admin_login']))
-    {
-        $ad_email=$_POST['ad_email'];
-        $ad_pwd=sha1(md5($_POST['ad_pwd']));//double encrypt to increase security
-        $stmt=$mysqli->prepare("SELECT ad_email ,ad_pwd , ad_id FROM his_admin WHERE ad_email=? AND ad_pwd=? ");//sql to log in user
-        $stmt->bind_param('ss',$ad_email,$ad_pwd);//bind fetched parameters
-        $stmt->execute();//execute bind
-        $stmt -> bind_result($ad_email,$ad_pwd,$ad_id);//bind result
-        $rs=$stmt->fetch();
-        $_SESSION['ad_id']=$ad_id;//Assign session to admin id
-        //$uip=$_SERVER['REMOTE_ADDR'];
-        //$ldate=date('d/m/Y h:i:s', time());
-        if($rs)
-            {//if its sucessfull
-                header("location:his_admin_dashboard.php");
-            }
-
-        else
-            {
-            #echo "<script>alert('Access Denied Please Check Your Credentials');</script>";
-                $err = "Access Denied Please Check Your Credentials";
-            }
-    }
+	session_start();
+	include('assets/inc/config.php');
+		if(isset($_POST['admin_sup']))
+		{
+			$ad_fname=$_POST['ad_fname'];
+			$ad_lname=$_POST['ad_lname'];
+			$ad_email=$_POST['ad_email'];
+			$ad_pwd=sha1(md5($_POST['ad_pwd']));//double encrypt to increase security
+            //sql to insert captured values
+			$query="insert into his_admin (ad_fname, ad_lname, ad_email, ad_pwd) values(?,?,?,?)";
+			$stmt = $mysqli->prepare($query);
+			$rc=$stmt->bind_param('ssss', $ad_fname, $ad_lname, $ad_email, $ad_pwd);
+			$stmt->execute();
+			/*
+			*Use Sweet Alerts Instead Of This Fucked Up Javascript Alerts
+			*echo"<script>alert('Successfully Created Account Proceed To Log In ');</script>";
+			*/ 
+			//declare a varible which will be passed to alert function
+			if($stmt)
+			{
+				$success = "Created Account Proceed To Log In";
+			}
+			else {
+				$err = "Please Try Again Or Try Later";
+			}
+			
+			
+		}
 ?>
+<!--End Server Side-->
 <!--End Login-->
 <!DOCTYPE html>
 <html lang="en">
     
 <head>
         <meta charset="utf-8" />
-        <title>Hospital Management System -A Super Responsive Information System</title>
+        <title>Hospital Management Information System -A Super Responsive Information System</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="" name="description" />
-        <meta content="" name="MartDevelopers" />
+        <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+        <meta content="Coderthemes" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <!-- App favicon -->
         <link rel="shortcut icon" href="assets/images/favicon.ico">
@@ -44,7 +49,6 @@
         <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
         <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" />
         <!--Load Sweet Alert Javascript-->
-        
         <script src="assets/js/swal.js"></script>
         <!--Inject SWAL-->
         <?php if(isset($success)) {?>
@@ -71,8 +75,6 @@
 
         <?php } ?>
 
-
-
     </head>
 
     <body class="authentication-bg authentication-bg-pattern">
@@ -86,35 +88,39 @@
                             <div class="card-body p-4">
                                 
                                 <div class="text-center w-75 m-auto">
-                                    <a href="index.php">
+                                    <a href="his_admin_register.php">
                                         <span><img src="assets/images/logo-dark.png" alt="" height="22"></span>
                                     </a>
-                                    <p class="text-muted mb-4 mt-3">Enter your email address and password to access admin panel.</p>
+                                    <p class="text-muted mb-4 mt-3">Don't have an account? Create your account, it takes less than a minute</p>
                                 </div>
 
-                                <form method='post' >
+                                <form  method='post'>
 
-                                    <div class="form-group mb-3">
+                                    <div class="form-group">
+                                        <label for="fullname">First Name</label>
+                                        <input class="form-control" type="text"  name = "ad_fname" id="fullname" placeholder="Enter your name" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="fullname">Last Name</label>
+                                        <input class="form-control" type="text" name="ad_lname" id="fullname" placeholder="Enter your name" required>
+                                    </div>
+                                    <div class="form-group">
                                         <label for="emailaddress">Email address</label>
-                                        <input class="form-control" name="ad_email" type="email" id="emailaddress" required="" placeholder="Enter your email">
+                                        <input class="form-control" name="ad_email" type="email" id="emailaddress" required placeholder="Enter your email">
                                     </div>
-
-                                    <div class="form-group mb-3">
+                                    <div class="form-group">
                                         <label for="password">Password</label>
-                                        <input class="form-control" name="ad_pwd" type="password" required="" id="password" placeholder="Enter your password">
+                                        <input class="form-control" name="ad_pwd" type="password" required id="password" placeholder="Enter your password">
                                     </div>
-
+                                    
                                     <div class="form-group mb-0 text-center">
-                                        <button class="btn btn-primary btn-block" name="admin_login" type="submit"> Admin Log In </button>
+                                        <button class="btn btn-primary btn-block" name="admin_sup" type="submit"> Sign Up </button>
                                     </div>
 
                                 </form>
-
-                                <!--
-                                For Now Lets Disable This 
-                                This feature will be implemented on later versions
+                                <!--Lets Disable This For We tryna implement it in later versions of this system
                                 <div class="text-center">
-                                    <h5 class="mt-3 text-muted">Sign in with</h5>
+                                    <h5 class="mt-3 text-muted">Sign up using</h5>
                                     <ul class="social-list list-inline mt-3 mb-0">
                                         <li class="list-inline-item">
                                             <a href="javascript: void(0);" class="social-list-item border-primary text-primary"><i class="mdi mdi-facebook"></i></a>
@@ -129,7 +135,7 @@
                                             <a href="javascript: void(0);" class="social-list-item border-secondary text-secondary"><i class="mdi mdi-github-circle"></i></a>
                                         </li>
                                     </ul>
-                                </div> 
+                                </div>
                                 -->
 
                             </div> <!-- end card-body -->
@@ -138,8 +144,7 @@
 
                         <div class="row mt-3">
                             <div class="col-12 text-center">
-                                <p> <a href="his_admin_pwd_reset.php" class="text-white-50 ml-1">Forgot your password?</a></p>
-                               <!-- <p class="text-white-50">Don't have an account? <a href="his_admin_register.php" class="text-white ml-1"><b>Sign Up</b></a></p>-->
+                                <p class="text-white-50">Already have account?  <a href="index.php" class="text-white ml-1"><b>Sign In</b></a></p>
                             </div> <!-- end col -->
                         </div>
                         <!-- end row -->
@@ -152,8 +157,9 @@
         </div>
         <!-- end page -->
 
-
-        <?php include ("assets/inc/footer1.php");?>
+        <!--Footer-->
+            <?php include("assets/inc/footer1.php");?>
+        <!-- End Footer-->
 
         <!-- Vendor js -->
         <script src="assets/js/vendor.min.js"></script>
